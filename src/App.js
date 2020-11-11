b62987b7c192116190f498fe69855406bff69df6
Karen Ardila <karen.ardila1893@gmail.com>
2020-11-10 23:36:18 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React, { useState } from 'react';
+import UserForm from './components/form';
+
+const App = () => {
+  return (
+   <UserForm/>
+  );
+}
+
+export default App;
